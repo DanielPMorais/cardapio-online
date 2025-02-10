4ce5995a4a31cbdc0cd08fd846c4671a8546f0c9
DanielPMorais <daniel.patrickmorais@gmail.com>
2025-02-10 18:24:15 -0300

    `Refactor buttons and links in index.html and app.js for dynamic functionality`

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ cardapio.eventos = {
 
     init: () => {
         cardapio.metodos.obterItensCardapio();
+        cardapio.metodos.carregarBotaoReserva();
+        cardapio.metodos.carregarBotaoReserva();
     }
 
 }
@@ -518,6 +520,41 @@ cardapio.metodos = {
             })
 
         }
+
+    },
+    
+    // carrega o link do botao reserva
+    carregarBotaoReserva: () => {
+
+        let texto = 'Olá! gostaria de gazer uma *reserva*';
+
+        let encode = encodeURI(texto);
+        let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+        $("#btnReserva").attr('href', URL);
+
+    },
+
+    // carrega o botao de ligar
+    carregarBotaoLigar: () => {
+
+        $("#btnLigar").attr('href', `tel${CELULAR_EMPRESA}`);
+
+    },
+
+    // abre o depoimento
+    abrirDepoimento: (depoimento) => {
+
+        $("#depoimento-1").addClass('hidden');
+        $("#depoimento-2").addClass('hidden');
+        $("#depoimento-3").addClass('hidden');
+
+        $("#btnDepoimento-1").removeClass('active');
+        $("#btnDepoimento-2").removeClass('active');
+        $("#btnDepoimento-3").removeClass('active');
+
+        $("#depoimento-" + depoimento).removeClass('hidden');
+        $("#btnDepoimento-" + depoimento).addClass('active');
 
     },
 
